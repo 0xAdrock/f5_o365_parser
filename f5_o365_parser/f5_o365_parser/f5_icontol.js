4a52d683e728feedead6b1a6f7440096c13bca9a
David Homoney <david@homoney.com>
2017-03-20 11:45:56 -0500

    Updated to create JSON for iControl call.

--- f5_o365_parser/f5_o365_parser/f5_icontol.js
+++ f5_o365_parser/f5_o365_parser/f5_icontol.js
@@ -1,17 +1,33 @@
 ﻿var F5rest = require('node-rest-client').Client;
+var fs = require('fs');
+var address = ('https://192.168.1.21/mgmt/tm/security/firewall/address-list/test_al');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var options = {
     user: "admin",
-    password: "Puto!puto1",
+    password: "admin",
     mimetypes: {
         json: ["application/json", "application/json;charset=utf-8"]
+    },
+    headers: { "Content-Type": "application/json" },
+    data: {
+        "addresses": [{ "name": "192.168.1.100" }, { "name": "10.0.1.0/24" }, { "name": "254.254.254.254" }]
     }
+
 };
 var f5rest = new F5rest(options);
-f5rest.get("https://192.168.2.2/mgmt/tm/net/self/", function (data, response) {
-    // console.log(data);
-    console.log("Logging Response Data\n\n");
-    console.log(response);
+f5rest.get(address, function (data, response) {
+    data = JSON.stringify(data);
+    console.log(data + "\n\n\n\n");
+    //console.log("Logging Response Data\n\n");
+    //console.log(response);
+    fs.writeFileSync("./data/f5_return.json", data);
+
+});
+
+
+f5rest.patch(address, options, function (data, response) {
+
+    console.log(data);
 
 });
--- f5_o365_parser/f5_o365_parser/main.js
+++ f5_o365_parser/f5_o365_parser/main.js
@@ -1,10 +1,111 @@
-﻿var getXML = require('./getxml');
-var parse = require('./parseandstore');
-var file = '../data/rawxml.xml';
+var https = require('https');
+var fs = require('fs');
+var ipregex = require('ip-regex');
+var lr = require('line-reader');
+var file = './data/rawxml.xml';
+var ipv4Addresses = [];
+var ipv6Addresses = [];
+var domains = [];
 
-getXML();
-parse();
 
-console.log("Number of IPv4 Addresses: " + parse.ipv4Addresses.length);
-console.log("Number of IPv6 Addresses: " + parse.ipv6Addresses.length);
-console.log("Number of URL Addresses: " + parse.domains.length);
+
+//HTTP Request Options
+var options = {
+    host: 'support.content.office.net',
+    port: '443',
+    path: '/en-us/static/O365IPAddresses.xml'
+};
+
+// Callback function to deal with response
+var getXML = function (response) {
+    // Continuously update stream with data
+    var body = '';
+    response.on('data', function (data) {
+        body += data;
+    });
+    response.on('end', function () {
+        // Data received completely
+        // Parse the XML received from MS into JSON
+        console.log('\n\nDone Reading XML. Now writing to file.\n\n');
+        //console.log(body);
+        fs.writeFileSync("./data/rawxml.xml", body);
+        console.log("Done Writing to file\n\n");
+        processXML(file);
+        
+        //console.log("Address:", jsondata.address);
+    });
+}
+
+
+
+// Now make the request
+var req = https.get(options, getXML);
+//req.end(processXML(file));
+
+/*
+
+The following function will read the XML line by line. It will first parse out
+the data of last update. This will be used in another function to see if we need
+to update the address list from Office 365. It will then initerate through the
+XML and parse out all the IPv4, IPv6, and DNS names into seperate arrays.
+*/
+
+function processXML(xmlfile) {
+    console.log("Reading XML\n\n");
+
+    lr.eachLine(xmlfile, function (line, last) {
+
+        var regDate = /"(.*?)"/;
+        var regAddress = /\<address\>(.*?)\<\/address\>/;
+        var regURL = /https?\:\/\/(.*?)/;
+        //console.log(line);
+
+        if (last) {
+            console.log("End of File\n\n");
+            console.log("Number of IPv4 Addresses: " + ipv4Addresses.length);
+            console.log("Number of IPv6 Addresses: " + ipv6Addresses.length);
+            console.log("Number of URL Addresses: " + domains.length);
+            //console.log("\n\nJSON of Array\n\n")
+            //console.log(JSON.stringify(domains));
+            console.log("\n\n\n\n");
+            console.log("IPv4 Addresses Formatted:\n\n");
+            console.log(JSON.stringify(ipv4Addresses));
+            return false;
+        }
+
+        if (line.includes("updated")) {
+            var upDate = line.match(regDate);
+            var upDateStr = upDate[1];
+            console.log(upDateStr);
+            console.log("Last Updated: " + upDateStr);
+            var upDateStr = Date.parse(upDateStr);
+            console.log("Parsed Date: " + upDateStr);
+        } else if (line.includes("<address>")) {
+            var addressArray = line.match(regAddress);
+            var address = addressArray[1];
+            //console.log("IPv6 Address: " + address);
+            if (ipregex.v4().test(address)) {
+                //console.log("IPv4 Address: " + address);
+                ipv4Addresses.push("{ \"name\": \"" + address + "\" }");
+            } else if (ipregex.v6().test(address)) {
+                //console.log("IPv6 Address: " + address);
+                ipv6Addresses.push("{ \"name\": \"" + address + "\" }");
+            } else {
+                //console.log("Domain Address " + address + " ignored");
+                if (address.includes("http")) {
+                    console.log("Found URL with HTTP in it " + address);
+                    var urlArray = address.match(regURL);
+                    console.log("urlArray: " + urlArray + " Length: " + urlArray.length);
+                    var url = urlArray[1];
+                    domains.push(address);
+                } else {
+                    domains.push(address);
+                }
+            }
+        }
+
+    });
+
+
+}
+
